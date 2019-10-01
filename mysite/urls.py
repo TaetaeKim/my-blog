40e6ba448343a5223a5a98119df08060f51ae8dc
TaetaeKim <ty980407@gmail.com>
2019-10-01 11:06:33 +0900

    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    # path('accounts/login/', views.login, name='login'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    # path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/logout/', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
