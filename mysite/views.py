ef269f455a0fe39f959a7594617339eca3ee6812
TaetaeKim <ty980407@gmail.com>
2019-10-08 00:02:40 +0900

    .

--- mysite/views.py
+++ mysite/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 
 def homepage(request):
-    return HttpResponse('<h1>여기는 218230085 김태연 홈페이지!</h1>'
+    return HttpResponse('<h1>여기는 218230085 김태연 홈페이지~!</h1>'
                         '<p>이전에 진행해본 프로젝트가 있어서 url을 변경하여(example/) 업로드하였습니다!</p>'
                         '<p>감사합니다. ^^</p>')
